d43395c82428f7ad360dd9762d698d2344aa8d72
Richard Herrera <rich@doctyper.com>
2010-04-17 14:28:27 -0700

    You can now choose between loading Ajax or toggling a div when selecting a tab.

--- js/ui/tab-view.js
+++ js/ui/tab-view.js
@@ -72,55 +72,78 @@ Dup.UI.TabView = (function (object, properties) {
 			heading.firstChild.nodeValue = title;
 		},
 		
-		loadPage : function(src, header, target) {
+		loadSection : function(src, header, target) {
 			if ($self.vars.xhr) {
 				$self.vars.xhr.abort();
 			}
 			
-			$self.vars.xhr = new XMLHttpRequest();
-			var xhr = $self.vars.xhr;
-			
-			xhr.onreadystatechange = function() {
-				if (xhr.readyState === 4) {
-					if (xhr.status === 200 || xhr.status === 304) {
-						
-						while (target.firstChild) {
-							target.removeChild(target.firstChild);
-						}
-						
-						var fragment = document.createDocumentFragment();
-						var dummy = document.createElement("div");
-						dummy.innerHTML = xhr.responseText;
-						
-						var title = dummy.querySelector(".ui-dup-tab-view-content");
-						
-						if (title) {
-							title = title.getAttribute("data-header-text");
-							$self.utils.updateHeader(header, title);
-						}
-						
-						for (var i = 0, j = dummy.childNodes.length; i < j; i++) {
-							var node = dummy.childNodes[i];
-							
-							if (node) {
-								fragment.appendChild(dummy.childNodes[i]);
-							}
-						}
-						
-						target.appendChild(fragment);
-						
-						$space.utils.Rivet.utils.checkScroll(null, "0", null, target);
-						
-					} else {
-						console.log(xhr.status);
+			if (src.charAt(0) === "#") {
+				var active = target.querySelector(src),
+				    children, title;
+				
+				if (active) {
+					title = active.getAttribute("data-header-text");
+					$self.utils.updateHeader(header, title);
+					
+					children = active.parentNode.querySelectorAll($space.utils.parseClass(".", "tab-view-content"));
+					
+					for (var i = 0, j = children.length; i < j; i++) {
+						$space.utils.addClass(children[i], $space.utils.parseClass("tab-hidden"));
 					}
 					
-					$space.utils.Rivet.utils.enableRivet();
-					delete $self.vars.xhr;
+					$space.utils.removeClass(active, $space.utils.parseClass("tab-hidden"));
 				}
-			};
-			xhr.open("GET", src, true);
-			xhr.send();
+
+				$space.utils.Rivet.utils.checkScroll(null, "0", null, target);
+				$space.utils.Rivet.utils.enableRivet();
+			} else {
+				
+				$self.vars.xhr = new XMLHttpRequest();
+				var xhr = $self.vars.xhr;
+
+				xhr.onreadystatechange = function() {
+					if (xhr.readyState === 4) {
+						if (xhr.status === 200 || xhr.status === 304) {
+
+							while (target.firstChild) {
+								target.removeChild(target.firstChild);
+							}
+
+							var fragment = document.createDocumentFragment();
+							var dummy = document.createElement("div");
+							dummy.innerHTML = xhr.responseText;
+
+							var title = dummy.querySelector(".ui-dup-tab-view-content");
+
+							if (title) {
+								title = title.getAttribute("data-header-text");
+								$self.utils.updateHeader(header, title);
+							}
+
+							for (var i = 0, j = dummy.childNodes.length; i < j; i++) {
+								var node = dummy.childNodes[i];
+
+								if (node) {
+									fragment.appendChild(dummy.childNodes[i]);
+								}
+							}
+
+							target.appendChild(fragment);
+
+							$space.utils.Rivet.utils.checkScroll(null, "0", null, target);
+
+						} else {
+							console.log(xhr.status);
+						}
+
+						$space.utils.Rivet.utils.enableRivet();
+						delete $self.vars.xhr;
+					}
+				};
+				xhr.open("GET", src, true);
+				xhr.send();
+				
+			}
 		}
 	};
 	
@@ -142,7 +165,8 @@ Dup.UI.TabView = (function (object, properties) {
 		    header = parent.querySelector("header"),
 		    footer = parent.querySelector("footer"),
 		    tabs = footer.querySelectorAll("li a"),
-		    active = footer.querySelector("li.ui-dup-tab-active a");
+		    active = footer.querySelector("li.ui-dup-tab-active a"),
+		    link = active.getAttribute("rel");
 		
 		for (var i = 0, j = tabs.length; i < j; i++) {
 			var tab = tabs[i], _parent,
@@ -150,6 +174,7 @@ Dup.UI.TabView = (function (object, properties) {
 			
 			tab.addEventListener("touchend", function(e) {
 				_parent = this.parentNode;
+				link = this.getAttribute("rel");
 				
 				if (!$space.utils.hasClass(_parent, _active)) {
 					for (var k = 0, l = tabs.length; k < l; k++) {
@@ -159,13 +184,13 @@ Dup.UI.TabView = (function (object, properties) {
 					$space.utils.Rivet.utils.disableRivet();
 
 					$space.utils.addClass(_parent, _active);
-					$self.utils.loadPage(this.getAttribute("rel"), header, content);
+					$self.utils.loadSection(link, header, content);
 				}
 			}, false);
 		}
 		
 		if (active) {
-			$self.utils.loadPage(active.getAttribute("rel"), header, content);
+			$self.utils.loadSection(link, header, content);
 		}
 		
 		window.addEventListener("orientationchange", function() {
